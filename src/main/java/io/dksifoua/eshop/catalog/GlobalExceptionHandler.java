e79d27c431a9c6eb505543a85e5601965d8af9b9
dksifoua <dimitri.sifoua@gmail.com>
2024-02-28 23:40:33 -0500

    Fix codecov.

--- src/main/java/io/dksifoua/eshop/catalog/GlobalExceptionHandler.java
+++ src/main/java/io/dksifoua/eshop/catalog/GlobalExceptionHandler.java
@@ -20,12 +20,12 @@ public class GlobalExceptionHandler implements WebExceptionHandler {
     public Mono<Void> handleDuplicateKeyException(ServerWebExchange exchange, MongoWriteException cause) {
         String type = cause.getError().getCategory().name();
         Matcher matcher = Pattern.compile("dup key: \\{.+}'").matcher(cause.getLocalizedMessage());
-        String payload = matcher.find()
-                ? matcher.group(0)
+        String payload = "Duplicate entry!!";
+        if (matcher.find()) {
+            payload = matcher.group(0)
                     .replace("dup key: ", "")
-                    .replace("'", "")
-                : cause.getLocalizedMessage();
-        System.out.println(payload);
+                    .replace("'", "");
+        }
         byte[] responseBody = String.format("{'type': %s, 'payload': %s}", type, payload).getBytes();
         exchange.getResponse().setStatusCode(HttpStatus.CONFLICT);
         exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
@@ -34,9 +34,6 @@ public class GlobalExceptionHandler implements WebExceptionHandler {
 
     @Override
     public @Nonnull Mono<Void> handle(@Nonnull ServerWebExchange exchange, Throwable ex) {
-        return switch (ex.getCause()) {
-            case MongoWriteException e -> handleDuplicateKeyException(exchange, e);
-            default -> throw new IllegalStateException("Unexpected value: " + ex.getLocalizedMessage());
-        };
+        return handleDuplicateKeyException(exchange, (MongoWriteException) ex.getCause());
     }
 }
