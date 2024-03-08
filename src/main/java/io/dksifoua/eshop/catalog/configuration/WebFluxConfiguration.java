3f833aff12a31d7422a34394ee5a60cd085bd9b4
dksifoua <dimitri.sifoua@gmail.com>
2024-03-08 04:30:17 -0500

    Allowed localhost cross-origin

---
+++ src/main/java/io/dksifoua/eshop/catalog/configuration/WebFluxConfiguration.java
@@ -0,0 +1,22 @@
+package io.dksifoua.eshop.catalog.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebFluxConfiguration implements WebFluxConfigurer {
+
+    @Value("${app.cors.allow.origins}")
+    private List<String> allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
